092fe12ad0d6df6d02343cb4c2c476eb9213d5c4
HanJi <a896522002@yahoo.com.tw>
2022-03-29 00:32:30 +0800

    test data.

--- facebook_crawlers/config/config.py
+++ facebook_crawlers/config/config.py
@@ -4,8 +4,8 @@ class TestData:
     LOGIN_URL = 'https://www.facebook.com/'
     LOGIN_PAGE_TITLE = 'Facebook - 登入或註冊'
 
-    GROUP_URL = 'https://www.facebook.com/GamesofaCareers'
-    GROUP_PAGE_TITLE = 'Gamesofa Careers'
+    PAGE_URL = 'https://www.facebook.com/GamesofaCareers'
+    PAGE_TITLE = '慧邦科技'
 
     ARTICLE_LINK = 'posts'
-    CLICK = '點到了'
+    CLICK = '讚'
