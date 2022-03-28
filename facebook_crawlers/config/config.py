b81668ef989eb2173611b7176dcef2fc48afd90f
HanJi <a896522002@yahoo.com.tw>
2022-03-28 18:40:43 +0800

    test function data.

---
+++ facebook_crawlers/config/config.py
@@ -0,0 +1,11 @@
+class TestData:
+    DRIVER_PATH = r'C:\Users\han\Desktop\facebook-crawlers\chromedriver.exe'
+
+    LOGIN_URL = 'https://www.facebook.com/'
+    LOGIN_PAGE_TITLE = 'Facebook - 登入或註冊'
+
+    GROUP_URL = 'https://www.facebook.com/GamesofaCareers'
+    GROUP_PAGE_TITLE = 'Gamesofa Careers'
+
+    ARTICLE_LINK = 'posts'
+    CLICK = '點到了'
